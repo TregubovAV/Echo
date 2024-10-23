2605fa80cf9ecadb0d54d20be1b8a34f666be458
TregubovAV <5595@rambler.ru>
2024-10-23 21:42:23 +0300

    Initial commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,29 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.given;
+
+class PostmanEchoTest {
+
+    @Test
+            void shouldReturnSendData () {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when().log().all()
+                .post("/post")
+// Проверки
+                .then().log().all()
+                .statusCode(200)
+                .body("data", org.hamcrest.Matchers.equalTo("some data"));
+
+
+
+    };
+
+
+
+}
